9157dc1100386487b6b15aa81a65aae7ae124163
Magnus <magnus.thore@gmail.com>
2019-06-20 22:26:01 +0200

    nu kan man skjuta med right bumper

---
+++ Cooldown Reload/Assets/Player/Scripts/Firing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firing : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+    }
+
+    private void Shoot() {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
