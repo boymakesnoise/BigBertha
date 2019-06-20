873d81c724969e252426394aa73e1dd8f869cbe2
Magnus <magnus.thore@gmail.com>
2019-06-20 19:54:18 +0200

    vapnet vänder sig åt rätt håll beroende på vart man siktar

--- Cooldown Reload/Assets/Player/Scripts/AimGun.cs
+++ Cooldown Reload/Assets/Player/Scripts/AimGun.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AimGun : MonoBehaviour
 {
+    private bool facingRight = true;
+    private float angles;
+
     void Update()
     {
         var h = Input.GetAxis("xAim");
@@ -13,5 +16,30 @@ public class AimGun : MonoBehaviour
             var newRot = Quaternion.AngleAxis(angle, Vector3.forward);
             transform.rotation = Quaternion.Lerp(transform.rotation, newRot, Time.deltaTime * 5);
         }
+
+        //angles = transform.eulerAngles.z;
+        //Flip(angles);
+
+        if (transform.eulerAngles.z > 90 && transform.eulerAngles.z < 270 && facingRight) {
+            facingRight = false;
+            Vector3 theScale = transform.localScale;
+            theScale.y *= -1;
+            transform.localScale = theScale;
+        }
+        if (transform.eulerAngles.z <= 90 || transform.eulerAngles.z >= 270) {
+            if (!facingRight) {
+                facingRight = true;
+                Vector3 theScale = transform.localScale;
+                theScale.y *= -1;
+                transform.localScale = theScale;
+            }
+        }
+        print(facingRight);
     }
+
+    //private void Flip(float angles) {
+    //    if (angles > 90 && angles < 270 && facingRight) {
+    //        facingRight = !facingRight;
+    //    }
+    //}
 }
