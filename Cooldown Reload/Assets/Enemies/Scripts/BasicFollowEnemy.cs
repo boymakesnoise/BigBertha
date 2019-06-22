6f69194d43081df0d159a51cd2e76982cfefa71e
Sportbrother <33148279+Sportbrother@users.noreply.github.com>
2019-06-22 13:30:54 +0200

    Fiender, beteenden, Anton-scen
    Var ett pucko och gjorde allt i 3D i mitt första projekt så har omarbetat scriptsen för att funka med 2D. Vet inte om det blev lika snygga lösningar, kanske bara bökigare, men får ta en titt på det senare. Lade till min egen scen.

---
+++ Cooldown Reload/Assets/Enemies/Scripts/BasicFollowEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicFollowEnemy : MonoBehaviour
+{
+
+    private Transform target;
+
+    public int moveSpeed = 4;
+    public int maxDist = 10;
+    public int minDist = 1;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        //transform.right = target.position - transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+    }
+
+}
---
+++ Cooldown Reload/Assets/Enemies/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.forward * speed;
+    }
+}
