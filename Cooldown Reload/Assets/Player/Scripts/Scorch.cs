68952312da4c8a0f0a8c13e699e85c379e4c21cb
Magnus <magnus.thore@gmail.com>
2019-06-23 14:08:00 +0200

    scorch dyker upp EFTER skott

---
+++ Cooldown Reload/Assets/Player/Scripts/Scorch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorch : MonoBehaviour
+{
+    public int spawnTime;
+    private SpriteRenderer spriten;
+
+    private void Awake() {
+        spriten = GetComponent<SpriteRenderer>();
+        spriten.enabled = false;
+    }
+
+    void Update() {
+        if (spawnTime > 0) {
+            spawnTime -= 1;
+        }
+
+        if (spawnTime <= 0) {
+            spriten.enabled = true;
+        }
+    }
+}
